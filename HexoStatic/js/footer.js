8bfab1c5953886c94ed5346f6d3b33b197381cb9
stardust14 <1137094865@qq.com>
2020-10-30 11:02:42 +0800

    修改名称

--- HexoStatic/js/footer.js
+++ HexoStatic/js/footer.js
@@ -1,3 +1,3 @@
 $(document).ready(function(e){
-    $('.copyright').html('©2020 <i class="fa-fw fas fa-heart card-announcement-animation cc_pointer"></i> shadow');
+    $('.copyright').html('©2020 <i class="fa-fw fas fa-heart card-announcement-animation cc_pointer"></i> stardust');
 })
